c7e573cd997f13def59eb598dbe070d22230be0b
Philipp Psurek <philipp.psurek@gmail.com>
2014-11-23 15:41:45 +0100

    site.mk: modify packages and version

--- site.mk
+++ site.mk
@@ -1,14 +1,20 @@
 GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
+	gluon-announced \
 	gluon-autoupdater \
-	gluon-config-mode \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
 	gluon-next-node \
-	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-status-page \
@@ -16,7 +22,12 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.4+0-exp$(shell date '+%Y%m%d')
+
+#DEFAULT_GLUON_RELEASE := 0.6~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.5.91-ffw-20141122
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+
